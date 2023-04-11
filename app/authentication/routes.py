bd985045fa7c70c9782ed5a63f2dab56d4a6bed2
savmcintyree <82894172+savmcintyree@users.noreply.github.com>
2023-04-11 01:37:46 -0500

    added motto

---
+++ app/authentication/routes.py
@@ -0,0 +1,8 @@
+from forms import UserLoginForm
+from models import User, db, check_password_hash
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+
+# imports for flask login 
+from flask_login import login_user, logout_user, LoginManager, current_user, login_required
+
+auth = Blueprint('auth', __name__, template_folder='auth_templates')
